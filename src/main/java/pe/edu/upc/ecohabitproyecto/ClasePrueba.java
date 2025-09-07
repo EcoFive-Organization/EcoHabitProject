84b220610b3ad4ae22c4dc15a8249f34dd071f19
Luis910-vpn <u202319510@upc.edu.pe>
2025-09-06 20:16:30 -0500

    commit

--- src/main/java/pe/edu/upc/ecohabitproyecto/ClasePrueba.java
+++ src/main/java/pe/edu/upc/ecohabitproyecto/ClasePrueba.java
@@ -2,8 +2,6 @@ package pe.edu.upc.ecohabitproyecto;
 
 public class ClasePrueba {
     public static void main(String[] args) {
-        System.out.println("Este es un mensaje de prueba");
-        System.out.println("Mensaje desde la rama-prueba");
-        System.out.println("Mensaje de la rama main");
+        System.out.println("Hola Mundo");
     }
 }
