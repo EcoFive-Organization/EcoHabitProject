9c4be3f696a27d85a765bc92042e34a68c834a18
Luis910-vpn <u202319510@upc.edu.pe>
2025-09-06 19:15:57 -0500

    coomit prueba

---
+++ src/main/java/pe/edu/upc/ecohabitproyecto/EcoHabitProyectoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.ecohabitproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoHabitProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoHabitProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/ecohabitproyecto/EcoHabitProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.ecohabitproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoHabitProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
