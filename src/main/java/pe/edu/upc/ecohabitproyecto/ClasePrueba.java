2364e6912889529b01b67bcdf4818a5feab55e8f
LuisCamus <u202319510@upc.edu.pe>
2025-09-06 23:51:58 -0500

    commit de prueba

--- src/main/java/pe/edu/upc/ecohabitproyecto/ClasePrueba.java
+++ src/main/java/pe/edu/upc/ecohabitproyecto/ClasePrueba.java
@@ -2,7 +2,19 @@ package pe.edu.upc.ecohabitproyecto;
 
 public class ClasePrueba {
     public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
         System.out.println("Mensaje desde la rama luis");
         System.out.println("Segundo mensaje de la rama luis");
+
+
+
     }
 }
