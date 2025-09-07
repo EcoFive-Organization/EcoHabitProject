71375c0a955e705e2bcf5533c15915325257b468
LuisCamus <u202319510@upc.edu.pe>
2025-09-06 23:49:11 -0500

    commit de prueba

--- src/main/java/pe/edu/upc/ecohabitproyecto/ClasePrueba.java
+++ src/main/java/pe/edu/upc/ecohabitproyecto/ClasePrueba.java
@@ -3,6 +3,6 @@ package pe.edu.upc.ecohabitproyecto;
 public class ClasePrueba {
     public static void main(String[] args) {
         System.out.println("Este es un mensaje de prueba");
-        System.out.println("Mensaje desde la rama-prueba");
+        System.out.println("Mensaje desde la rama-diego");
     }
 }
