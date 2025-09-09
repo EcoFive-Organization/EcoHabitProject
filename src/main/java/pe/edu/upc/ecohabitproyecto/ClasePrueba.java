90f1f7d2455cb6625bc99154177b2272f8887b62
andre <u202010729@upc.edu.pe>
2025-09-09 00:35:25 -0500

    commit de prueba

--- src/main/java/pe/edu/upc/ecohabitproyecto/ClasePrueba.java
+++ src/main/java/pe/edu/upc/ecohabitproyecto/ClasePrueba.java
@@ -3,8 +3,8 @@ package pe.edu.upc.ecohabitproyecto;
 public class ClasePrueba {
     public static void main(String[] args) {
 
-        System.out.println("Mensaje desde la rama luis");
-        System.out.println("Segundo mensaje de la rama luis");
+        System.out.println("Mensaje desde la rama andres");
+        System.out.println("Segundo mensaje de la rama a");
         System.out.println("Mensaje desde la rama-diego");
     }
 }
