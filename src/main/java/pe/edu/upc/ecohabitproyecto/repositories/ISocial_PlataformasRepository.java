dc3b8cadae6b989677989800fc421fbd3a238748
Diego Aguirre <u202319891@upc.edu.pe>
2025-09-15 21:47:08 -0500

    Eliminación de tablas innecesarias

--- src/main/java/pe/edu/upc/ecohabitproyecto/repositories/ISocial_PlataformasRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.ecohabitproyecto.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.ecohabitproyecto.entities.Social_Plataformas;
-
-@Repository
-public interface ISocial_PlataformasRepository extends JpaRepository<Social_Plataformas, Integer> {
-}
