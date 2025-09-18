57731d511e0dda0b4246a0f34cc11d3e959987c0
andre <u202010729@upc.edu.pe>
2025-09-17 21:07:03 -0500

    commit de andres

--- src/main/java/pe/edu/upc/ecohabitproyecto/repositories/IConsumoRepository.java
+++ src/main/java/pe/edu/upc/ecohabitproyecto/repositories/IConsumoRepository.java
@@ -3,8 +3,8 @@ package pe.edu.upc.ecohabitproyecto.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pe.edu.upc.ecohabitproyecto.entities.Dispositivo;
+import pe.edu.upc.ecohabitproyecto.entities.Consumo;
 
 @Repository
-public interface IConsumoRepository extends JpaRepository<Dispositivo,Integer> {
+public interface IConsumoRepository extends JpaRepository<Consumo,Integer> {
 }
