8d65420c9d68ff1c247fbde4f6a3046c99ca9eca
kevin-ayala <u201821781@upc.edu.pe>
2025-09-23 05:02:30 -0500

    commit de prueba

--- src/main/java/pe/edu/upc/ecohabitproyecto/repositories/ILogroRepository.java
+++ src/main/java/pe/edu/upc/ecohabitproyecto/repositories/ILogroRepository.java
@@ -3,8 +3,10 @@ package pe.edu.upc.ecohabitproyecto.repositories;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.ecohabitproyecto.entities.Logro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 @Repository
 public interface ILogroRepository extends JpaRepository<Logro, Integer> {
+    Optional<Logro> findByNombre(String nombre);
 
 }
--- src/main/java/pe/edu/upc/ecohabitproyecto/servicesimplements/LogroServiceImplement.java
+++ src/main/java/pe/edu/upc/ecohabitproyecto/servicesimplements/LogroServiceImplement.java
@@ -1,15 +1,28 @@
 package pe.edu.upc.ecohabitproyecto.servicesimplements;
 
 import pe.edu.upc.ecohabitproyecto.entities.Logro;
+import pe.edu.upc.ecohabitproyecto.entities.Usuario;
+import pe.edu.upc.ecohabitproyecto.entities.UsuarioLogro;
 import pe.edu.upc.ecohabitproyecto.repositories.ILogroRepository;
+import pe.edu.upc.ecohabitproyecto.repositories.IUsuarioLogroRepository;
+import pe.edu.upc.ecohabitproyecto.repositories.IUsuarioRepository;
 import pe.edu.upc.ecohabitproyecto.servicesinterfaces.ILogroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class LogroServiceImplement implements ILogroService {
+    @Autowired
+    private ILogroRepository logroRepo;
+
+    @Autowired
+    private IUsuarioLogroRepository userLogroRepo;
+
+    @Autowired
+    private IUsuarioRepository usuarioRepo;
 
     @Autowired
     private ILogroRepository repo;
@@ -17,6 +30,29 @@ public class LogroServiceImplement implements ILogroService {
     @Override
     public List<Logro> list() {
         return repo.findAll();
+    }
+
+    @Override
+    public void desbloquearLogro(Integer idUsuario, String nombreLogro) {
+        // 1. Verificar usuario
+        Usuario usuario = usuarioRepo.findById(idUsuario)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        // 2. Buscar logro
+        Logro logro = logroRepo.findByNombre(nombreLogro)
+                .orElseThrow(() -> new RuntimeException("Logro no existe"));
+
+        // 3. Verificar si ya desbloqueado
+        if (userLogroRepo.existsByUsuario_IdUsuarioAndIdLogro(idUsuario, logro.getIdLogro())) {
+            throw new RuntimeException("Logro ya desbloqueado");
+        }
+
+        // 4. Crear relación UsuarioLogro
+        UsuarioLogro ul = new UsuarioLogro();
+        ul.setUsuario(usuario);
+        ul.setIdLogro(logro.getIdLogro());
+        ul.setFechaObtenido(LocalDateTime.now());
+        userLogroRepo.save(ul);
     }
 
     @Override
