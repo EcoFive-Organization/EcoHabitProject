37139a78b0529d4d7fe7ad0c44df0d05442fafc3
andre <u202010729@upc.edu.pe>
2025-09-22 20:51:12 -0500

    commit de andres

--- src/main/java/pe/edu/upc/ecohabitproyecto/repositories/IAlertaRepository.java
+++ src/main/java/pe/edu/upc/ecohabitproyecto/repositories/IAlertaRepository.java
@@ -1,9 +1,17 @@
 package pe.edu.upc.ecohabitproyecto.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.ecohabitproyecto.entities.Alerta;
 
+import java.util.List;
+
 @Repository
 public interface IAlertaRepository extends JpaRepository<Alerta,Integer> {
+    @Query(value = "SELECT a.id_alerta,a.fecha,a.mensaje,a.id_consumo,d.ubicacion\n" +
+            "FROM alerta a\n" +
+            "JOIN consumo c ON a.id_consumo = c.id_consumo\n" +
+            "JOIN dispositivo d ON c.id_dispositivo = d.id_dispositivo;", nativeQuery = true)
+    List<Object[]> findAllByIdAlerta();
 }
--- src/main/java/pe/edu/upc/ecohabitproyecto/servicesinterfaces/IAlertaService.java
+++ src/main/java/pe/edu/upc/ecohabitproyecto/servicesinterfaces/IAlertaService.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface IAlertaService {
     public List<Alerta> list();
     public void insert(Alerta alerta);
+    List<Object[]> findAllByIdAlerta();
+
 }
