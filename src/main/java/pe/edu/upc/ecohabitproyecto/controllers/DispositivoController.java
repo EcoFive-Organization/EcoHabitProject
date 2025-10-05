502faf915f9afa9b2b63ab145fe10a646b418708
andre <u202010729@upc.edu.pe>
2025-10-05 00:28:03 -0500

    commit de andres

--- src/main/java/pe/edu/upc/ecohabitproyecto/controllers/DispositivoController.java
+++ src/main/java/pe/edu/upc/ecohabitproyecto/controllers/DispositivoController.java
@@ -5,6 +5,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.ecohabitproyecto.dtos.DispositivoDTOInsert;
 import pe.edu.upc.ecohabitproyecto.dtos.DispositivoDTOList;
@@ -15,6 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@PreAuthorize("hasAuthority('ADMIN')")
 @RequestMapping("/dispositivos")
 public class DispositivoController {
     @Autowired
