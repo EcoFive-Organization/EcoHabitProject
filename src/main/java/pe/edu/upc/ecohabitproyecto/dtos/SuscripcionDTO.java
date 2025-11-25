c4fff08055ee24745034698069b884a02f94be7d
LuisCamus <u202319510@upc.edu.pe>
2025-11-24 21:38:53 -0500

    Corrección tabla SuscripcionDTO

--- src/main/java/pe/edu/upc/ecohabitproyecto/dtos/SuscripcionDTO.java
+++ src/main/java/pe/edu/upc/ecohabitproyecto/dtos/SuscripcionDTO.java
@@ -1,16 +1,17 @@
 package pe.edu.upc.ecohabitproyecto.dtos;
 
+import pe.edu.upc.ecohabitproyecto.entities.PlanSuscripcion;
 import pe.edu.upc.ecohabitproyecto.entities.Usuario;
 
 import java.sql.Date;
 
 public class SuscripcionDTO {
     private int idSuscripcion;
-    private String plan;
     private Date fechaInicio;
     private Date fechaFin;
     private String estado;
     private Usuario usuario;
+    private PlanSuscripcion planSuscripcion;
 
     public int getIdSuscripcion() {
         return idSuscripcion;
@@ -18,14 +19,6 @@ public class SuscripcionDTO {
 
     public void setIdSuscripcion(int idSuscripcion) {
         this.idSuscripcion = idSuscripcion;
-    }
-
-    public String getPlan() {
-        return plan;
-    }
-
-    public void setPlan(String plan) {
-        this.plan = plan;
     }
 
     public Date getFechaInicio() {
@@ -58,5 +51,13 @@ public class SuscripcionDTO {
 
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
+    }
+
+    public PlanSuscripcion getPlanSuscripcion() {
+        return planSuscripcion;
+    }
+
+    public void setPlanSuscripcion(PlanSuscripcion planSuscripcion) {
+        this.planSuscripcion = planSuscripcion;
     }
 }
